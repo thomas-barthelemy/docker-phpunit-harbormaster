de298688967690eb6ff0eadbf238ca114ee03576
Thomas Barthelemy <thomasb@gobeta.com.cn>
2015-09-11 17:13:19 +0700

    updated entrypoint to avoid running phpunit if migration fails

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,21 @@ service php5-fpm restart
 service apache2 restart
 
 cd /code/DeliveryWeb
+
+# Use an existing cache that will be mapped as a volume to avoid re-downloading packages
 export COMPOSER_CACHE_DIR=/root/composer/cache
 export COMPOSER_HOME=/root/composer
 composer install --no-interaction
 
+# Creating the database
 sudo -u postgres psql -c "create user vagrant with password 'vagrant'"
 sudo -u postgres psql -c "create database symfony"
 
+# No errors tolerated here!
+set -e
+
+# Running migrations
 php app/console doctrine:migrations:migrate --no-interaction
 
+# Running PHPUnit
 phpunit -c app/phpunit.xml.dist
-
