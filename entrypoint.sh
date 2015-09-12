bb0cf06af4e3cab74353e08f41c7d83e446c1b6a
Thomas Barthelemy <thomasb@gobeta.com.cn>
2015-09-12 10:17:10 +0700

    updated entrypoint for composer to prefer-dist

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,9 @@ cd /code/DeliveryWeb
 # Use an existing cache that will be mapped as a volume to avoid re-downloading packages
 export COMPOSER_CACHE_DIR=/root/composer/cache
 export COMPOSER_HOME=/root/composer
-composer install --no-interaction
+# We use --prefer-dist to download zip files instead of cloning repositories
+# this allow caching for "commit" requirements (@dev).
+composer install --no-interaction --prefer-dist
 
 # Creating the database
 sudo -u postgres psql -c "create user vagrant with password 'vagrant'"
